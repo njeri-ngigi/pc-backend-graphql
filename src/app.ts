e8e3912bc7c511130b74d3e665d7176637212d0a
Shalon N. Ngigi <njery.ngigi@gmail.com>
2022-09-20 15:27:11 +0300

    initial setup: - add eslint, typescript, node server

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import config from "./configs";
+
+const { PORT } = config;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  return console.log(`Express is listening at http://localhost:${PORT}`);
+});
